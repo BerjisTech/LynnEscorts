c65d25e6c740616a423cdc1bfb44d40ec368893f
BerjisTech <berjistechnologies@gmail.com>
2020-05-08 15:09:39 +0300

    Initial commit

---
+++ app/src/main/java/tech/berjis/lynnescorts/ChangePhoneNumber.java
@@ -0,0 +1,29 @@
+package tech.berjis.lynnescorts;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hbb20.CountryCodePicker;
+
+public class ChangePhoneNumber extends AppCompatActivity {
+
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_phone_number);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangePhoneNumber.super.finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/tech/berjis/lynnescorts/CommentsActivity.java
@@ -0,0 +1,26 @@
+package tech.berjis.lynnescorts;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CommentsActivity extends AppCompatActivity {
+
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CommentsActivity.super.finish();
+            }
+        });
+    }
+}
