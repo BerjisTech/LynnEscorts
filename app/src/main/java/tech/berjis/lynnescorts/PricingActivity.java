67da12b8fa4e2a999343a60bc9b2a6618512e694
BerjisTech <berjistechnologies@gmail.com>
2020-05-09 13:11:50 +0300

    Profile update

---
+++ app/src/main/java/tech/berjis/lynnescorts/PricingActivity.java
@@ -0,0 +1,14 @@
+package tech.berjis.lynnescorts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PricingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pricing);
+    }
+}
