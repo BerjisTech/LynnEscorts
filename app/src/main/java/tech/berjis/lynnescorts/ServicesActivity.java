764c40d270dd6b7f0b59cb1c31aba5da0fedecc5
BerjisTech <berjistechnologies@gmail.com>
2020-05-09 20:27:10 +0300

    Services complete

--- app/src/main/java/tech/berjis/lynnescorts/ServicesActivity.java
+++ app/src/main/java/tech/berjis/lynnescorts/ServicesActivity.java
@@ -1,5 +1,6 @@
 package tech.berjis.lynnescorts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,8 +11,11 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ServicesActivity extends AppCompatActivity {
     ImageView back;
@@ -21,6 +25,7 @@ public class ServicesActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     DatabaseReference dbRef;
+    String UID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class ServicesActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         dbRef = FirebaseDatabase.getInstance().getReference();
+        UID = mAuth.getCurrentUser().getUid();
         dbRef.keepSynced(true);
 
         back = findViewById(R.id.back);
@@ -43,21 +49,88 @@ public class ServicesActivity extends AppCompatActivity {
         photosVideos = findViewById(R.id.photosVideos);
         videoCalls = findViewById(R.id.videoCalls);
 
+        loadServices();
+        saveService();
+
+    }
+
+    private void loadServices() {
+        dbRef.child("Services").child(UID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child("InOutCalls").exists()) {
+                    String serviceValue = dataSnapshot.child("InOutCalls").getValue().toString();
+
+                    if (serviceValue.equals("Yes, I offer both")) {
+                        inCallRadio.check(R.id.bothCalls);
+                    }
+                    if (serviceValue.equals("I ONLY offer INCALLS")) {
+                        inCallRadio.check(R.id.inCalls);
+                    }
+                    if (serviceValue.equals("I ONLY offer OUTCALLS")) {
+                        inCallRadio.check(R.id.outCalls);
+                    }
+                    if (serviceValue.equals("No, I don't offer any incalls or outcalls.")) {
+                        inCallRadio.check(R.id.noCall);
+                    }
+
+                }
+
+                if (dataSnapshot.child("Media").exists()) {
+                    String serviceValue = dataSnapshot.child("Media").getValue().toString();
+
+                    if (serviceValue.equals("Yes, I sell both photos and videos")) {
+                        photosVideos.check(R.id.bothMedia);
+                    }
+                    if (serviceValue.equals("I ONLY sell PHOTOS")) {
+                        photosVideos.check(R.id.photos);
+                    }
+                    if (serviceValue.equals("I ONLY sell VIDEOS")) {
+                        photosVideos.check(R.id.videos);
+                    }
+                    if (serviceValue.equals("No, I don't sell digital media")) {
+                        photosVideos.check(R.id.noMedia);
+                    }
+                }
+
+                if (dataSnapshot.child("VideoCalls").exists()) {
+                    String serviceValue = dataSnapshot.child("VideoCalls").getValue().toString();
+
+                    if (serviceValue.equals("Yes, I do video calls with any paying clients")) {
+                        videoCalls.check(R.id.yesVideoCalls);
+                    }
+                    if (serviceValue.equals("No, I don't do video calls")) {
+                        videoCalls.check(R.id.noVideoCalls);
+                    }
+                }
+
+                if (!dataSnapshot.child("InOutCalls").exists() ||
+                        !dataSnapshot.child("Media").exists() ||
+                        dataSnapshot.child("VideoCalls").exists()) {
+                    dbRef.child("Services").child(UID).child("InOutCalls").setValue("Yes, I offer both");
+                    dbRef.child("Services").child(UID).child("Media").setValue("Yes, I sell both photos and videos");
+                    dbRef.child("Services").child(UID).child("VideoCalls").setValue("Yes, I do video calls with any paying clients");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void saveService() {
         inCallRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 int selectedId = inCallRadio.getCheckedRadioButtonId();
                 inCallRadioBtn = findViewById(selectedId);
-                Toast.makeText(ServicesActivity.this, inCallRadioBtn.getText(), Toast.LENGTH_SHORT).show();
-                /*switch(checkedId)
-                {
-                    case R.id.firstRadioButton:
-                        //Implement logic
-                        break;
-                    case R.id.secondRadioButton:
-                        //Implement logic
-                        break;
-                }*/
+                String selected = inCallRadioBtn.getText().toString();
+
+                dbRef.child("Services").child(UID).child("InOutCalls").setValue(selected);
+
+                //Toast.makeText(ServicesActivity.this, inCallRadioBtn.getText(), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -66,16 +139,11 @@ public class ServicesActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 int selectedId = photosVideos.getCheckedRadioButtonId();
                 photosVideosBtn = findViewById(selectedId);
-                Toast.makeText(ServicesActivity.this, photosVideosBtn.getText(), Toast.LENGTH_SHORT).show();
-                /*switch(checkedId)
-                {
-                    case R.id.firstRadioButton:
-                        //Implement logic
-                        break;
-                    case R.id.secondRadioButton:
-                        //Implement logic
-                        break;
-                }*/
+                String selected = photosVideosBtn.getText().toString();
+
+                dbRef.child("Services").child(UID).child("Media").setValue(selected);
+
+                //Toast.makeText(ServicesActivity.this, photosVideosBtn.getText(), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -84,16 +152,12 @@ public class ServicesActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 int selectedId = videoCalls.getCheckedRadioButtonId();
                 videoCallsBtn = findViewById(selectedId);
-                Toast.makeText(ServicesActivity.this, videoCallsBtn.getText(), Toast.LENGTH_SHORT).show();
-                /*switch(checkedId)
-                {
-                    case R.id.firstRadioButton:
-                        //Implement logic
-                        break;
-                    case R.id.secondRadioButton:
-                        //Implement logic
-                        break;
-                }*/
+                String selected = videoCallsBtn.getText().toString();
+
+                dbRef.child("Services").child(UID).child("VideoCalls").setValue(selected);
+
+
+                //Toast.makeText(ServicesActivity.this, videoCallsBtn.getText(), Toast.LENGTH_SHORT).show();
             }
         });
     }
